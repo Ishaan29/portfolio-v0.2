c35d5f1cda1d535ffea0a3c68856653fe6f0872e
Ishaan bajpai <bajpaiishan@yahoo.in>
2024-02-18 02:45:25 +0530

    renames titles

--- pages/signin/title.signin.tsx
+++ pages/signin/title.signin.tsx
@@ -8,7 +8,7 @@ export default function TitleSignin() {
             <meta charSet="utf-8" />
             <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
             <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
-            <meta name="description" content="Sign in to access Muhammad Fiaz's portfolio and explore my world of technology and development projects." />
+            <meta name="description" content="Sign in to access Eshaan Bajpai's portfolio and explore my world of technology and development projects." />
             {/*These are the Keywords that will Boost your SEO in Ranking, so Make Sure to include and Update it up to your preference or don't mind this! 😴*/}
             <meta name="keywords" content="Sign In, Login, Authentication, Portfolio Access, Eshaan Bajpai, Technology, Development, Coding" />
             <title>Sign in | Eshaan Bajpai</title>
